8873b2cf6fd6f5c020e35a7f7f3d554cce20675a
Andrew Pang <andrew.pang@workday.com>
2016-12-15 01:02:31 -0800

    RecyclerView with CardView working for feed

---
+++ app/src/main/java/com/example/andrewpang/dreamjournal/DreamEntry.java
@@ -0,0 +1,52 @@
+package com.example.andrewpang.dreamjournal;
+
+import java.util.Date;
+
+public class DreamEntry {
+    private String entry;
+    private Date date;
+    private boolean isPublic;
+
+    public DreamEntry() {
+    }
+
+    public DreamEntry(String entry, Date date, boolean isPublic) {
+        this.entry = entry;
+        this.date = date;
+        this.isPublic = isPublic;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    //TODO: Convert Date
+    public String getDateDay() {
+        return "08";
+    }
+
+    public String getDateMonth() {
+        return "Nov";
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+}
--- app/src/main/java/com/example/andrewpang/dreamjournal/FeedFragment.java
+++ app/src/main/java/com/example/andrewpang/dreamjournal/FeedFragment.java
@@ -2,9 +2,16 @@ package com.example.andrewpang.dreamjournal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FeedFragment extends Fragment {
 
@@ -14,8 +21,25 @@ public class FeedFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+        final RecyclerView feedRecyclerView = ButterKnife.findById(rootView, R.id.feed_recycler_view);
+        feedRecyclerView.setHasFixedSize(true);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(mockDreamEntries());
+        feedRecyclerView.setAdapter(recyclerViewAdapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        feedRecyclerView.setLayoutManager(llm);
+
+        return rootView;
+    }
+
+    private ArrayList<DreamEntry> mockDreamEntries() {
+        ArrayList<DreamEntry> dreamEntries = new ArrayList<>();
+        dreamEntries.add(new DreamEntry("Hey", Calendar.getInstance().getTime(), true));
+        dreamEntries.add(new DreamEntry("Hey this a dream aaslkdfjlask jdf;laksjdf;lashf;kj sdkfljl skdljf sdfjkl skdjlflj sdfkljsdj sldfkjsdflksjf slkdjflksdfjkldf slkdfjslkdjflk slkfjdkls aslfkj;asf a;sldkfj;slkafj lskdjfkldjsflkj", Calendar.getInstance().getTime(), true));
+        dreamEntries.add(new DreamEntry("Hey, nightmare man", Calendar.getInstance().getTime(), true));
+        dreamEntries.add(new DreamEntry("Hey", Calendar.getInstance().getTime(), true));
+        return dreamEntries;
     }
 
 
